b7135ff794e244890a5f6f4f5c7d475aee3cc60b
Evgeniy <nifpopnaf@gmail.com>
2021-07-22 18:22:00 +0400

    update model in modules

--- server/src/top-page/top-page.model.ts
+++ server/src/top-page/top-page.model.ts
@@ -1,27 +1,75 @@
-export enum TopLevelCategory{
-	Courses,
-	Services,
-	Books,
-	Products
+import { prop, index } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+export enum TopLevelCategory {
+  Courses,
+  Services,
+  Books,
+  Products,
 }
 
-export class TopPageModel {
-	_id: string;
-	firstCategory: TopLevelCategory;
-	secondCategory: string;
-	title: string;
-	category: string;
-	hh?: {
-		count: number;
-		juniorSalary: number;
-		middleSalary: number;
-		seniorSalary: number;
-	};
-	advantages: {
-		title: string;
-		description: string;
-	}[];
-	seoText: string;
-	tagsTitle: string;
-	tags: string[];
+export class HhData {
+  @prop()
+  count: number;
+
+  @prop()
+  juniorSalary: number;
+
+  @prop()
+  middleSalary: number;
+
+  @prop()
+  seniorSalary: number;
+
+  @prop()
+  updatedAt: Date;
+}
+
+export class TopPageAdvantage {
+  @prop()
+  title: string;
+
+  @prop()
+  description: string;
+}
+
+export interface TopPageModel extends Base {}
+
+@index({ '$**': 'text' })
+export class TopPageModel extends TimeStamps {
+  @prop({ enum: TopLevelCategory })
+  firstCategory: TopLevelCategory;
+
+  @prop()
+  secondCategory: string;
+
+  @prop({ unique: true })
+  alias: string;
+
+  @prop()
+  title: string;
+
+  @prop()
+  metaTitle: string;
+
+  @prop()
+  metaDescription: string;
+
+  @prop()
+  category: string;
+
+  @prop({ type: () => HhData })
+  hh?: HhData;
+
+  @prop({ type: () => [TopPageAdvantage] })
+  advantages?: TopPageAdvantage[];
+
+  @prop()
+  seoText?: string;
+
+  @prop()
+  tagsTitle: string;
+
+  @prop({ type: () => [String] })
+  tags: string[];
 }
