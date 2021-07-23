f05ba3642e29cf3a13904a1d038aa7b49b4dd518
Evgeniy <nifpopnaf@gmail.com>
2021-07-24 00:18:34 +0400

    add product controller and service

--- server/src/review/review.controller.ts
+++ server/src/review/review.controller.ts
@@ -11,6 +11,7 @@ import { Body,
 	ValidationPipe 
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { IdValidationPipe } from 'src/pipes/ad-validation.pipe';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
@@ -26,13 +27,15 @@ export class ReviewController {
 	}
 
 	@Get('byProduct/:productId')
-	async getByProduct(@Param('productId') productId: string) {
+	async getByProduct(
+		@Param('productId', IdValidationPipe) productId: string,
+	) {
 		return this.reviewService.findByProductId(productId);
 	}
 
 	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
-	async delete(@Param('id') id: string) {
+	async delete(@Param('id', IdValidationPipe) id: string) {
 		const deleteDoc = await this.reviewService.delete(id);
 		if (!deleteDoc) {
 			throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
