02a903280eb84ba67d6815faea88b71a2e078d3c
Evgeniy <nifpopnaf@gmail.com>
2021-07-25 21:58:07 +0400

    include static props and paths in courses/[alias]

--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,9 +1,12 @@
+import { GetStaticProps } from 'next';
 import React from 'react';
 import { Button, Htag } from '../components';
 import { Layout, withLayout } from '../layout/Layout';
+import axios from 'axios'
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element{
-  return (
+function Home({ menu, firstCategory }: HomeProps): JSX.Element {
+	return (
 		<>
 			<Htag tag="h1">Текст</Htag>
 			<Button appearance="primary" arrow="right">
@@ -12,9 +15,33 @@ function Home(): JSX.Element{
 			<Button appearance="ghost" arrow="down">
 				Кнопка
 			</Button>
+			<ul>
+				{menu.map((el) => (
+					<li key={el._id.secondCategory}>{el._id.secondCategory}</li>
+				))}
+			</ul>
 		</>
 	)
 }
 
 
 export default withLayout(Home)
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0
+	const { data: menu } = await axios.post<MenuItem[]>(
+		process.env.NEXT_PUBLIC_DOMAIN + "/top-page/find",
+		{ firstCategory }
+	)
+	return {
+		props: {
+			menu,
+			firstCategory,
+		},
+	}
+}
+
+interface HomeProps extends Record<string, unknown>{
+	menu: MenuItem[],
+	firstCategory: number
+}
