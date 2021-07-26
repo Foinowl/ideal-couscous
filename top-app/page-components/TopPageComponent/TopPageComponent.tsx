4a4082368d78ec595ad18f957e292ef4c074d4d1
Evgeniy <nifpopnaf@gmail.com>
2021-07-26 23:18:13 +0400

    create advantages comp

--- top-app/page-components/TopPageComponent/TopPageComponent.tsx
+++ top-app/page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,8 +1,9 @@
-import { Htag, P, Tag } from "../../components"
+import { Advantages, Htag, P, Tag } from "../../components"
 import { TopPageComponentProps } from "./TopPageComponent.props"
 import styles from "./TopPageComponent.module.css"
 import { HhData } from "../../components/HhData/HhData"
 import { TopLevelCategory } from "../../interfaces/page.interface"
+import React from 'react'
 
 export const TopPageComponent = ({
 	page,
@@ -29,7 +30,27 @@ export const TopPageComponent = ({
 					hh.ru
 				</Tag>
 			</div>
-			{firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
+			{firstCategory == TopLevelCategory.Courses && page.hh && (
+				<HhData {...page.hh} />
+			)}
+			{page.advantages && page.advantages.length > 0 && (
+				<>
+					<Htag tag="h2">Преимущства</Htag>
+					<Advantages advantages={page.advantages} />
+				</>
+			)}
+			{page.seoText && (
+				<div
+					className={styles.seo}
+					dangerouslySetInnerHTML={{ __html: page.seoText }}
+				/>
+			)}
+			<Htag tag="h2">Получаемые навыки</Htag>
+			{page.tags.map((t) => (
+				<Tag key={t} color="primary">
+					{t}
+				</Tag>
+			))}
 		</div>
 	)
 }
